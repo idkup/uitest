21a2ff42e977cc899e7a1c71f5bb0e591abe96c9
idkup <idkup137@gmail.com>
2024-03-09 15:37:38 -0500

    test

--- src/App.js
+++ src/App.js
@@ -5,16 +5,12 @@ export interface Env {
 	db: D1Database;
 }
 
-function importAll(r) {
-  return r.keys().map(r);
-}
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-		const images = importAll(require.context('./', false, /\.(png|jpe?g|svg)$/));
+		const images = require.context('./graphics', true);
         <img src={logo} className="App-logo" alt="logo" />
         const stmt = await env.db.prepare('SELECT * FROM mons').run()
 		stmt.forEach(element => {
